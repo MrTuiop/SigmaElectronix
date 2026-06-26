9a00208226414f51f6b864a107102bcfd03725dd
Tuiop Tuiop <tuiop7777@mail.ru>
2026-06-26 10:57:30 +0300

    Связь UiTranslations с Languages через LanguageCode
    Добавлена миграция, создающая индекс по UiTranslations.LanguageCode и внешний ключ на Languages.Code. В Down эти изменения удаляются.

---
+++ SigmaElectronix.Server/Migrations/20260625164000_LinkUiTranslationsToLanguages.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SigmaElectronix.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkUiTranslationsToLanguages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UiTranslations_LanguageCode",
+                table: "UiTranslations",
+                column: "LanguageCode");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UiTranslations_Languages_LanguageCode",
+                table: "UiTranslations",
+                column: "LanguageCode",
+                principalTable: "Languages",
+                principalColumn: "Code",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UiTranslations_Languages_LanguageCode",
+                table: "UiTranslations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UiTranslations_LanguageCode",
+                table: "UiTranslations");
+        }
+    }
+}
